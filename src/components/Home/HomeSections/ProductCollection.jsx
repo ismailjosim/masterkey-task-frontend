b0e9ab51122912971527d91123992fa8ad4ca9d3
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-02-18 18:26:06 +0600

    add side product

--- src/components/Home/HomeSections/ProductCollection.jsx
+++ src/components/Home/HomeSections/ProductCollection.jsx
@@ -13,35 +13,57 @@ const ProductCollection = () => {
 
 
     return (
-        <div className='w-11/12 mx-auto mt-10 grid grid-cols-4 gap-5'>
-            {
-                products.slice(0, 3).map((product, idx) => {
+        <section className='grid grid-cols-3 gap-5'>
+            <div className='col-span-2 mt-10 grid grid-cols-3 gap-5'>
+                {
+                    products.slice(0, 3).map((product, idx) => {
 
-                    return (
-                        <div key={ idx } class="shadow-lg rounded-md">
-                            <div class="flex justify-center">
-                                <img src={ product?.picture } alt="" class="w-20"
-                                />
-                            </div>
-                            <div class="mt-3">
-                                <h3 className='text-xl font-medium'>{ product.name }</h3>
-                                <p>price: <span>${ product.price }</span></p>
-                                <div className='flex justify-between items-center p-2'>
-                                    <p className='flex items-center justify-center gap-2 font-semibold text-primary'>
-                                        <img src={ start } alt="" />
-                                        <span>{ product.ratting }</span>
-                                    </p>
-                                    <p className='bg-primary p-1 rounded-full'>
-                                        <img src={ plus } alt="" />
-                                    </p>
+                        return (
+                            <div key={ idx } class="shadow-lg rounded-md">
+                                <div class="flex justify-center">
+                                    <img src={ product?.picture } alt="" class="w-20"
+                                    />
+                                </div>
+                                <div class="mt-3">
+                                    <h3 className='text-xl font-medium'>{ product.name }</h3>
+                                    <p>price: <span>${ product.price }</span></p>
+                                    <div className='flex justify-between items-center p-2'>
+                                        <p className='flex items-center justify-center gap-2 font-semibold text-primary'>
+                                            <img src={ start } alt="" />
+                                            <span>{ product.ratting }</span>
+                                        </p>
+                                        <p className='bg-primary p-1 rounded-full'>
+                                            <img src={ plus } alt="" />
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='col-span-1 absolute right-10'>
+                <div className='flex items-center justify-between mx-2'>
+                    <h3 className='text-xl font-semibold'>Daily Deals</h3>
+                    <p>view All</p>
+                </div>
+                { products.map((product, idx) => {
+                    return (
+                        <div key={ idx } className='flex items-center justify-between mb-2'>
+                            <div>
+                                <img width={ 50 } src={ product?.picture } alt="" />
+                            </div>
+                            <div>
+                                <h4>{ product.name }</h4>
+                                <span className='text-xs'>{ product.ratting } reviews and { product.order } orders</span>
+                            </div>
+                            <div>price ${ product.price }</div>
                         </div>
                     )
                 })
-            }
-
-        </div>
+                }
+            </div>
+        </section>
     );
 };
 
